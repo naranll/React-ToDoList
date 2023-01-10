1725fb531c3d4d4d82771b7c3f132b84d6263d6c
Morning <morning@admins-iMac.local>
2023-01-10 12:22:44 +0800

    commit

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+/* function useState(input){
+      function setInput(x){   
+         input = x;                 //store current value of x in input
+      }
+    window.reload();
+    return [input, setInput];        //the value and the only function that can change it
+}
+*/
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const [num, setNum] = useState(0);
+  // let newText;
+  // let boxNum = [];
+
+  // const changeInput = (txt) => {
+  //   // console.log(txt.target.value);
+  //   newText = txt.target.value;
+  // }
+
+  // // function consoleInput() {
+  // //   console.log(newText);
+  // //   setInputText(newText);
+  // // }
+
+  // function boxCounter() {
+  //   boxNum.push(num);
+  //   setNum(num + 1);
+  // }
+
+  // return (
+  //   <div className="App">
+  //     <div className='inputTodo'>
+  //       <form>
+  //         <input type="text" placeholder='enter text' onChange={changeInput}></input>
+  //       </form>
+  //       {/* <button onClick={() => {
+  //         createBox();
+  //         console.log(boxNum);
+  //         boxNum.map((count) => {
+  //           return (<div key={count} className="tasks">o</div>)
+  //         });
+  //       }}>Enter</button> */}
+  //     </div>
+
+  //   </div>
+  // );
 }
 
 export default App;
